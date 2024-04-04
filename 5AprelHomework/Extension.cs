d3a55be5bbb4c66c3f91085df58b2f8212e8fc75
Samir <Samir@DESKTOP-AHQSCFO>
2024-04-04 08:28:05 -0700

    Add project files.

---
+++ 5AprelHomework/Extension.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5AprelHomework
+{
+    public static class Extension
+    {
+
+        public static bool IsPrime(this int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            else if (number == 2)
+            {
+                return true;
+            }
+            for (int i = 2; i*i < number; i++)
+            {
+                
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static int Faktorial(this int faktorial)
+        {
+            int a = 1;
+            for (int i = 1; i <= faktorial; i++)
+            {
+                a = i * a;
+            }
+            return a;
+        }
+        public static double Fahrenheit(this double fahrenheit)
+        {
+            double C;
+            C = (fahrenheit - 32) * 5 / 9;
+            return C;
+        }
+        public static double Celsius(this double celsius)
+        {
+            double F;
+            F = celsius * 9/5 + 32;
+            return F;
+        }
+            
+    }
+}
---
+++ 5AprelHomework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5AprelHomework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!")
+            int number = 99;
+            Console.WriteLine(number.IsPrime());
+            int faktorial = 5;
+            Console.WriteLine(faktorial.Faktorial());
+            double fahrenneit = 8;
+            Console.WriteLine(fahrenneit.Fahrenheit());
+            double celsie = 5;
+            Console.WriteLine(celsie.Celsius());
+        }
+    }
+}
